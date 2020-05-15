2fce3a6c8a8975901357061cccf2024536d76d98
hina <hina@192.168.0.12>
2020-05-15 17:58:25 +0900

    G18948

---
+++ kei/src/kei/likefood.java
@@ -0,0 +1,9 @@
+   package kei;
+public class likefood {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("私の好きな食べ物は抹茶です！どんな料理でも美味しいからです");
+	}
+
+}
